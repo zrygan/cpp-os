11806d92647934b25bae3d4d80ef5a3f3f29bc7e
zrygan <zr.gntn@gmail.com>
2026-06-09 18:26:35 +0800

    SoC on gameloop

--- mockos/src/game_loop.h
+++ mockos/src/game_loop.h
@@ -1,11 +1,54 @@
 #pragma once
+#include "apps/info.h"
+#include "apps/task_manager.h"
+#include "apps/text_editor.h"
+#include "context.h"
 #include "imgui.h"
 #include "imgui_impl_glfw.h"
 #include "imgui_impl_opengl3.h"
+#include "interface/taskbar.h"
+#include <GLFW/glfw3.h>
+
 namespace mockos {
+
 inline void NewFrame() {
   ImGui_ImplOpenGL3_NewFrame();
   ImGui_ImplGlfw_NewFrame();
   ImGui::NewFrame();
 }
+
+inline void Update(mockos::Context *ctx, TaskManager &taskManager) {
+  mockos::GetTimeString(ctx);
+  taskManager.randomize();
+}
+
+inline void DrawUI(mockos::Context *ctx) {
+  mockos::MakeTaskbar(ctx);
+  if (ctx->flags.show_info)
+    mockos::MakeInfo(ctx);
+  if (ctx->flags.show_text_editor)
+    mockos::MakeTextEditor(ctx);
+}
+
+inline void Render(mockos::Context *ctx) {
+  ImGui::Render();
+  int display_w, display_h;
+  glfwGetFramebufferSize(ctx->window, &display_w, &display_h);
+  glViewport(0, 0, display_w, display_h);
+  glClearColor(0.1f, 0.1f, 0.1f, 1.0f);
+  glClear(GL_COLOR_BUFFER_BIT);
+  ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
+  glfwSwapBuffers(ctx->window);
+}
+
+inline void RunLoop(mockos::Context *ctx, TaskManager &taskManager) {
+  while (!glfwWindowShouldClose(ctx->window) && !ctx->flags.kill) {
+    glfwPollEvents();
+    NewFrame();
+    Update(ctx, taskManager);
+    DrawUI(ctx);
+    Render(ctx);
+  }
+}
+
 } // namespace mockos
--- mockos/src/main.cpp
+++ mockos/src/main.cpp
@@ -1,61 +1,16 @@
-#include "apps/info.h"
 #include "apps/task_manager.h"
-#include "apps/text_editor.h"
 #include "context.h"
 #include "game_loop.h"
-#include "interface/taskbar.h"
-#include <GLFW/glfw3.h>
 #include <iostream>
 
 int main() {
-  mockos::Context *this_ctx = mockos::Init();
+  mockos::Context *ctx = mockos::Init();
 
-  // @OutForMilks add this to a taskmanager init function
-  // so that we dont bloat up the main loop
-  // if needed, use inline for that function
+  // @OutForMilks move this into a TaskManager init function
   TaskManager taskManager;
   taskManager.initialize();
 
-  // ********************
-  // GAME LOOP
-  // ********************
-  while (!glfwWindowShouldClose(this_ctx->window) && !this_ctx->flags.kill) {
-
-    glfwPollEvents();
-    mockos::NewFrame();
-
-    // **********
-    // SYSTEM CLOCK
-    // **********
-    mockos::GetTimeString(this_ctx);
-
-    // **********
-    // TASKMANAGER STUFF
-    // **********
-    taskManager.randomize();
-    // **********
-    // INTERFACE STUFF
-    // **********
-    mockos::MakeTaskbar(this_ctx);
-
-    if (this_ctx->flags.show_info)
-      mockos::MakeInfo(this_ctx);
-
-    if (this_ctx->flags.show_text_editor)
-      mockos::MakeTextEditor(this_ctx);
-
-    ImGui::Render();
-    int display_w, display_h;
-    glfwGetFramebufferSize(this_ctx->window, &display_w, &display_h);
-    glViewport(0, 0, display_w, display_h);
-    glClearColor(0.1f, 0.1f, 0.1f, 1.0f);
-    glClear(GL_COLOR_BUFFER_BIT);
-
-    ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
-    glfwSwapBuffers(this_ctx->window);
-  }
-
-  mockos::Cleanup(this_ctx);
-
+  mockos::RunLoop(ctx, taskManager);
+  mockos::Cleanup(ctx);
   return 0;
 }
