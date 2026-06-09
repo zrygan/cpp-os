38564f5b29492a90caac8a27a832527bca70bf6f
zrygan <zr.gntn@gmail.com>
2026-06-09 17:45:10 +0800

    Added soft wrapping

--- mockos/src/apps/text_editor.h
+++ mockos/src/apps/text_editor.h
@@ -1,52 +1,39 @@
 #pragma once
-
 #include "imgui.h"
 #include "os.h"
 
 namespace mockos {
 
+const ImGuiInputTextFlags kTextEditorFlags = ImGuiInputTextFlags_WordWrap;
+
 inline void MakeTextEditor(mockos::OS *this_os) {
-  if (ImGui::Begin("Notepad.exe", &this_os->flags.show_text_editor,
-                   ImGuiWindowFlags_MenuBar)) {
+    if (ImGui::Begin("Notepad.exe", &this_os->flags.show_text_editor,
+                     ImGuiWindowFlags_MenuBar)) {
+        static char text_buffer[4096] =
+            "Welcome to the MockOS Text Editor!\n"
+            "You can click here and start typing freely.\n"
+            "It supports multiline wrapping, selections, and standard hotkeys.";
 
-    if (ImGui::BeginMenuBar()) {
-      if (ImGui::BeginMenu("File")) {
-        if (ImGui::MenuItem("New", "Ctrl+N")) {
+        if (ImGui::BeginMenuBar()) {
+            if (ImGui::BeginMenu("File")) {
+                if (ImGui::MenuItem("New", "Ctrl+N")) {
+                    text_buffer[0] = '\0';
+                }
+                ImGui::Separator();
+                if (ImGui::MenuItem("Exit")) {
+                    this_os->flags.show_text_editor = false;
+                }
+                ImGui::EndMenu();
+            }
+            ImGui::EndMenuBar();
         }
-        ImGui::Separator();
-        if (ImGui::MenuItem("Exit")) {
-          this_os->flags.show_text_editor = false;
-        }
-        ImGui::EndMenu();
-      }
-      if (ImGui::BeginMenu("Edit")) {
-        if (ImGui::MenuItem("Undo", "Ctrl+Z")) {
-        }
-        if (ImGui::MenuItem("Redo", "Ctrl+Y")) {
-        }
-        ImGui::Separator();
-        if (ImGui::MenuItem("Cut", "Ctrl+X")) {
-        }
-        if (ImGui::MenuItem("Copy", "Ctrl+C")) {
-        }
-        if (ImGui::MenuItem("Paste", "Ctrl+V")) {
-        }
-        ImGui::EndMenu();
-      }
-      ImGui::EndMenuBar();
+
+        ImGui::InputTextMultiline("##TextEditorArea", text_buffer,
+                                  IM_ARRAYSIZE(text_buffer),
+                                  ImGui::GetContentRegionAvail(),
+                                  kTextEditorFlags);
     }
-
-    static char text_buffer[4096] =
-        "Welcome to the MockOS Text Editor!\n"
-        "You can click here and start typing freely.\n"
-        "It supports multiline wrapping, selections, and standard hotkeys.";
-
-    ImVec2 input_size = ImVec2(-1.0f, -1.0f);
-
-    ImGui::InputTextMultiline("##TextEditorArea", text_buffer,
-                              IM_ARRAYSIZE(text_buffer), input_size);
-  }
-  ImGui::End();
+    ImGui::End();
 }
 
 } // namespace mockos
