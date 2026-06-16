c47f70c5bc9425c80d22291d6f25012b07f437ba
chuae <egtchua@gmail.com>
2026-06-17 01:10:22 +0800

    fixing controller loop

--- prosched/src/controller/Controller.cpp
+++ prosched/src/controller/Controller.cpp
@@ -1,9 +1,27 @@
+#include <stdio.h>
+#include <iostream>
+#include <string>
+#include <cctype>
+#include <algorithm>
+
 #include "Controller.h"
+
+using namespace std;
 
 void Controller::initialize() { this->view = View(); }
 
 void Controller::run() {
-  while (true) {
     view.DisplayMenu();
-  }
+    std::string input;
+
+    // while command not exit keep i/o open
+    while(input != "exit"){
+      std::transform(input.begin(), input.end(), input.begin(),
+      [](unsigned char c){ return std::tolower(c); });
+
+      std::cout << "Enter Input: ";
+      std::cin >> input;
+
+      // actions here
+    }
 }
--- prosched/src/main.cpp
+++ prosched/src/main.cpp
@@ -2,6 +2,7 @@
 #include <stdio.h>
 
 #include "config.h"
+#include "context.h"
 #include "controller/Controller.h"
 
 using namespace std;
@@ -10,7 +11,8 @@ int main() {
   printf("hello, world!\n");
 
   ConfigStruct *cs = fromFile();
-
+  // AlgoContext ctx = AlgoContext::buildConfig(cs);
+  
   if (cs) {
     std::cout << cs->batch_process_freq;
   }
