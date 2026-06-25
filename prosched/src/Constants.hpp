b8e2bc582f5a3a29dc9e556041c5fc9da61bf53b
cgkghj <goaaron09@gmail.com>
2026-06-25 12:05:59 +0800

    sleep time is now 0
    sadge

--- prosched/src/Constants.hpp
+++ prosched/src/Constants.hpp
@@ -3,5 +3,5 @@
 namespace prosched {
     constexpr int kMaxProcesses = 10;           // limit for scheduler-test
     constexpr int kNumPrintInstructions = 100;  // default prints per process
-    constexpr int kTickDurationMs = 10;         // duration of a single tick in milliseconds
+    constexpr int kTickDurationMs = 0;         // duration of a single tick in milliseconds
 }
