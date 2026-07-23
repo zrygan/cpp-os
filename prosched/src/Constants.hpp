3f86ec210b44ebba6deea98fa630ad82d7e5c092
zrygan <zr.gntn@gmail.com>
2026-07-23 16:55:29 +0800

    Screen c constants

--- prosched/src/Constants.hpp
+++ prosched/src/Constants.hpp
@@ -1,9 +1,13 @@
 #pragma once
 
+#include <cstddef>
+
 namespace prosched {
     constexpr int kMaxProcesses = 10;           // limit for scheduler-test
     constexpr int kNumPrintInstructions = 100;  // default prints per process
     constexpr int kTickDurationMs = 0;         // duration of a single tick in milliseconds
     constexpr long kMinProcessMemoryBytes = 64;    // 2^6
     constexpr long kMaxProcessMemoryBytes = 65536; // 2^16
+    constexpr std::size_t kMinUserInstructions = 1;
+    constexpr std::size_t kMaxUserInstructions = 50;
 }
